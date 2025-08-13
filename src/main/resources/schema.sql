32550f876afc322c3bf6262529b709de17782fc4
lucasdantas-xpe <lucas.dantas@estudante.faculdadexp.edu.br>
2025-08-12 22:36:14 -0300

    chore: add SQL schema for users table

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+create table if not exists users (
+  id uuid primary key,
+  name varchar(120) not null,
+  email varchar(160) not null unique,
+  login varchar(60)  not null unique,
+  password_hash varchar(100) not null,
+  address varchar(200),
+  role varchar(16) not null,
+  last_modified_at timestamp not null default now(),
+  active boolean not null default true
+);
